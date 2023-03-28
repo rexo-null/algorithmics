64a17e220abb52567f56ef5f9c096cd8b272d624
R3X0-NULL <jayden7deley@yandex.ru>
2023-03-28 23:59:04 +0300

    ...

--- Lesson 1/Quest 2/Quest 2/main.cpp
+++ Lesson 1/Quest 2/Quest 2/main.cpp
@@ -4,11 +4,11 @@
 
 int main() {
 	int start_point = 0;
-	int array[8] = {1, 8, 6, 7, 3, 45, 9, 6};
+	int array[8] = {1, 2, 2, 3, 3, 3, 9, 48};
 
 	std::cout << "Введите точку отсчёта: ";
 	std::cin >> start_point;
-	std::cout << "Количество элементов в массиве больших, чем " << start_point << ": " << values_count(array, 3, std::size(array));
+	std::cout << "Количество элементов в массиве больших, чем " << start_point << ": " << values_count(array, start_point, std::size(array));
 
 	return 0;
 }
--- Lesson 1/Quest 2/Quest 2/values_count.cpp
+++ Lesson 1/Quest 2/Quest 2/values_count.cpp
@@ -2,12 +2,38 @@
 
 int values_count(int *array, int start_point, int size) {
 	int count = 0;
+	int left_array = 0;
+	int right_array = size - 1;
 
-	for (int i = 0; i < size; i++) {
-		if (array[i] > start_point) {
-			count++;
+	while (left_array < right_array) {
+		int middle_array = (left_array + right_array) / 2;
+
+		if (start_point > array[middle_array]) {
+			left_array = middle_array + 1;
 		}
-	}
+
+		else if (start_point == array[middle_array]) {
+			int num_index = middle_array + 1;
+			int count_num = middle_array + 1;
+
+			while (true) {
+				if (start_point != array[num_index]) {
+					count = count_num;
+					break;
+				}
+				else {
+					num_index++;
+					count_num--;
+				}
+			}
+			break;
+		}
+
+		else if (start_point < array[middle_array]) {
+			count += middle_array + 1;
+			right_array = middle_array - 1;
+		}
+	};
 
 	return count;
 }
